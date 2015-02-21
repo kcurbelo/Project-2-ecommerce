c4786ee95e596b5c3f11bc5e8d3dabc5c8bd6568
kcurbelo <kevin.curbelo@gmail.com>
2015-02-21 11:41:34 -0800

    Remove S3 code

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,18 +35,9 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
 
-  #S3 
-  config.paperclip_defaults = {
-    :storage => :s3,
-    :s3_credentials => {
-      :bucket => ENV['S3_BUCKET_NAME'],
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-    }
-  }
 
 
-
+  
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
 
