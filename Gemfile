6adefec668ba3ca568b1daa2a8aba8c65a8de697
kcurbelo <kevin.curbelo@gmail.com>
2015-02-20 17:14:56 -0800

    precompiled assets locally

--- Gemfile
+++ Gemfile
@@ -28,6 +28,8 @@ gem 'bcrypt', '~> 3.1.7'
 
 gem "paperclip", "~> 4.2"
 
+gem 'rails_12factor'
+
 # Use Unicorn as the app server
 # gem 'unicorn'
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,6 +23,10 @@ Rails.application.configure do
   # Disable Rails's static asset server (Apache or NGINX will already do this).
   config.serve_static_assets = false
 
+  config.assets.compile = true
+  config.assets.digest = true
+
+
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
