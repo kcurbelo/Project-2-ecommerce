42f510efd6b51abc4a21ef282ea8e00b298b713a
kcurbelo <kevin.curbelo@gmail.com>
2015-02-22 17:19:10 -0800

    Added validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ActiveRecord::Base
   has_secure_password #made available from the bcrypt gem
   has_many :carts
   has_many :jackets, through: :carts
+
+  e = /\A([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)\z/
+
+  validates :f_name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, format: {with: e}
 end
